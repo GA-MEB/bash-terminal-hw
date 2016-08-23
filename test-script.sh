8f2f8dbf37de1f6ec5922f998ab2a68da81c3691
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-08-23 14:25:45 -0400

    Fix LICENSE
    Take out extra content from LICENSE file.
    Add GPL copyright statement to all code files.

--- test-script.sh
+++ test-script.sh
@@ -1,3 +1,16 @@
+# General Assembly, WDI (Web Development Immersive) Remote, Cohort 2
+# Copyright (C) 2016 Matt Brendzel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 if [ ! -f '.progress-record' ]
 then
   touch '.progress-record'
